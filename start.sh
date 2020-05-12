5f75ee017b2c19fcdf61b2e22dc39e3f2850e6d2
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-05-12 10:58:17 -0400

    Added OpenStack environment variables sharing with Docker container

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ echo "Building $repository..."
 docker build --tag "$repository:$tag" .
 
 echo "Running $repository..."
-docker run --publish 5000:5000 "$repository"
+docker run --env-file ./env.list --publish 5000:5000 "$repository"
