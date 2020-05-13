c45c2e2d9fd05ec2310fbc9a535348ec0133bd05
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-05-13 15:05:36 -0400

    Added error logging to terraform init & terraform apply

--- app/build_magic_castle.sh
+++ app/build_magic_castle.sh
@@ -3,5 +3,5 @@
 # Stop on first non-zero return code
 set -e
 
-terraform init -plugin-dir /root/.terraform.d/plugin-cache/linux_amd64 >> terraform_init.log
-terraform apply -auto-approve >> terraform_apply.log
+terraform init -no-color -plugin-dir /root/.terraform.d/plugin-cache/linux_amd64 > terraform_init.log 2>&1
+terraform apply -no-color -auto-approve > terraform_apply.log 2>&1
