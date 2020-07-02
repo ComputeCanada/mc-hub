03b388484db075ff2d302f0a2d5776ec492484aa
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-07-02 19:41:27 +0000

    Changed Plan created for Build cancelled status chip

--- frontend/src/components/ui/StatusChip.vue
+++ frontend/src/components/ui/StatusChip.vue
@@ -14,7 +14,7 @@ import ClusterStatusCode from "@/models/ClusterStatusCode";
 
 const ClusterFormattedStatus = Object.freeze({
   idle: { text: "Idle", color: "blue" },
-  created: { text: "Plan created", color: "blue" },
+  created: { text: "Build cancelled", color: "darkgrey" },
   plan_running: { text: "Creating plan", color: "orange" },
   build_running: { text: "Build running", color: "orange" },
   build_success: { text: "Build success", color: "green" },
