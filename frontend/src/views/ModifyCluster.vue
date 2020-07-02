6fb2ad586e92003d478d9a7025761d63da0f12be
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-07-02 19:32:41 +0000

    Fixed NavigationDuplicated error

--- frontend/src/views/ModifyCluster.vue
+++ frontend/src/views/ModifyCluster.vue
@@ -34,8 +34,11 @@ export default {
       /*
       Removes all query parameters from the url (e.g. "?destroy=1").
       The parameters still get sent as a prop to ClusterEditor.
+
+      NavigationDuplicated errors need to be caught, because vue-router throws this
+      exception whenever a path is navigated twice in a row, even with different query params.
       */
-      this.$router.push({});
+      this.$router.replace({ query: {} }).catch(() => {});
     }
   }
 };
