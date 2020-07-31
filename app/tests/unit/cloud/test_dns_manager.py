9694fd61d25198ce93b8772bc7dc1c6c93368e5b
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-07-31 13:24:23 +0000

    Added DNS manager tests

---
+++ app/tests/unit/cloud/test_dns_manager.py
@@ -0,0 +1,48 @@
+from tests.test_helpers import *
+from models.cloud.dns_manager import DnsManager
+import pytest
+
+
+def test_initialize_disallowed_domain():
+    with pytest.raises(KeyError):
+        DnsManager("invalid.com")
+
+
+def test_get_available_domains():
+    assert [
+        "calculquebec.cloud",
+        "c3.ca",
+        "sub.example.com",
+    ] == DnsManager.get_available_domains()
+
+
+def test_get_environment_variables_with_dns_provider():
+    assert DnsManager("calculquebec.cloud").get_environment_variables() == {
+        "CLOUDFLARE_API_TOKEN": "EXAMPLE_TOKEN",
+        "CLOUDFLARE_ZONE_API_TOKEN": "EXAMPLE_TOKEN",
+        "CLOUDFLARE_DNS_API_TOKEN": "EXAMPLE_TOKEN",
+    }
+
+
+def test_get_environment_variables_no_dns_provider():
+    assert DnsManager("sub.example.com").get_environment_variables() == {}
+
+
+def test_get_magic_castle_configuration_with_dns_provider():
+    assert DnsManager("calculquebec.cloud").get_magic_castle_configuration() == {
+        "dns": {
+            "email": "you@example.com",
+            "source": f"git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare",
+            "name": "${module.openstack.cluster_name}",
+            "domain": "${module.openstack.domain}",
+            "public_ip": "${module.openstack.ip}",
+            "login_ids": "${module.openstack.login_ids}",
+            "rsa_public_key": "${module.openstack.rsa_public_key}",
+            "ssh_private_key": "${module.openstack.ssh_private_key}",
+            "sudoer_username": "${module.openstack.sudoer_username}",
+        }
+    }
+
+
+def test_get_magic_castle_configuration_no_dns_provider():
+    assert DnsManager("sub.example.com").get_magic_castle_configuration() == {}
