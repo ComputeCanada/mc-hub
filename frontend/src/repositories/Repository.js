73dcc8270ea08569bcffe3c801b802f841e76cca
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-10-23 17:25:04 +0000

    Automatically refresh after session expired
    Vue Js now checks every API response for 302 redirects.
    If the response code is 302, the page is refreshed, leading the user to
    the SSO login page.

--- frontend/src/repositories/Repository.js
+++ frontend/src/repositories/Repository.js
@@ -1,4 +1,17 @@
 import axios from "axios";
 
 const baseURL = process.env.VUE_APP_API_URL || "/api";
-export default axios.create({ baseURL });
+const axiosInstance = axios.create({ baseURL });
+
+axiosInstance.interceptors.response.use((response)=> {
+    return response;
+}, (error) => {
+    if (error.response.status == 302) {
+        // API request was redirected to single sign-on login page, therefore the session expired
+        location.reload();
+    } else {
+        return Promise.reject(error);
+    }
+})
+
+export default axiosInstance;
