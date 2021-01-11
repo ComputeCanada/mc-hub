495393bdfd7560ff2ae31146ece0acf1e58e23c9
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-11 17:14:44 -0500

    Updated terraform required version in main.tf.json

--- app/models/magic_castle/magic_castle_configuration.py
+++ app/models/magic_castle/magic_castle_configuration.py
@@ -86,7 +86,7 @@ class MagicCastleConfiguration:
         """
 
         main_tf_configuration = {
-            "terraform": {"required_version": ">= 0.12.21"},
+            "terraform": {"required_version": ">= 0.13.4"},
             "module": {
                 "openstack": {
                     "source": f"{MAGIC_CASTLE_MODULE_SOURCE}//openstack?ref={MAGIC_CASTLE_VERSION_TAG}",
