3e11bb6ad147d0b70813551a56fdef2012964ef4
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-13 16:57:13 -0500

    Bumped MC to 9.2

--- app/models/constants.py
+++ app/models/constants.py
@@ -7,7 +7,7 @@ AUTO_ALLOCATED_IP_LABEL = "Automatic allocation"
 
 # Magic Castle
 MAGIC_CASTLE_MODULE_SOURCE = "git::https://github.com/ComputeCanada/magic_castle.git"
-MAGIC_CASTLE_VERSION_TAG = "9.1"
+MAGIC_CASTLE_VERSION_TAG = "9.2"
 
 # Paths and filenames
 CLUSTERS_PATH = path.join(environ["HOME"], "clusters")
