791f4833a59de3e63f7df65a69bcf243cf3a82ae
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-10-21 15:03:45 -0400

    Fix RUN_PATH when PWD does not exist

--- app/models/constants.py
+++ app/models/constants.py
@@ -1,4 +1,4 @@
-from os import path, environ
+from os import path, environ, getcwd
 
 # Regular constants
 INSTANCE_CATEGORIES = ["mgmt", "login", "node"]
@@ -14,11 +14,13 @@ MAGIC_CASTLE_VERSION_TAG = "11.6"
 TERRAFORM_REQUIRED_VERSION = ">= 0.15.2"
 
 # Paths and filenames
-RUN_PATH = environ.get("MCH_RUN_PATH", environ["PWD"])
-CLUSTERS_PATH = environ.get("MCH_CLUSTERS_PATH", path.join(RUN_PATH, "clusters"))
+RUN_PATH = environ.get("MCH_RUN_PATH", getcwd())
+CLUSTERS_PATH = environ.get(
+    "MCH_CLUSTERS_PATH", path.join(RUN_PATH, "clusters"))
 APP_PATH = environ.get("MCH_APP_PATH", path.join(RUN_PATH, "app"))
 DIST_PATH = environ.get("MCH_DIST_PATH", path.join(RUN_PATH, "dist"))
-DATABASE_PATH = environ.get("MCH_DATABASE_PATH", path.join(RUN_PATH, "database"))
+DATABASE_PATH = environ.get(
+    "MCH_DATABASE_PATH", path.join(RUN_PATH, "database"))
 SCHEMA_MIGRATIONS_DIRECTORY = path.join(APP_PATH, "database", "migrations")
 CONFIGURATION_FILE_PATH = environ.get("MCH_CONFIGURATION_FILE_PATH", RUN_PATH)
 
