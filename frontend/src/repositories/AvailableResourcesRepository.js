16387e71f9b70edd271559c2e755da005b8400f6
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-12-10 09:46:16 -0500

    Add cloud_id onChange to ui

--- frontend/src/repositories/AvailableResourcesRepository.js
+++ frontend/src/repositories/AvailableResourcesRepository.js
@@ -5,7 +5,14 @@ const resource = "/available-resources";
 export default {
   get(hostname = null) {
     if (hostname) {
-      return Repository.get(`${resource}/${hostname}`);
+      return Repository.get(`${resource}/host/${hostname}`);
+    } else {
+      return Repository.get(`${resource}`);
+    }
+  },
+  getCloud(cloud_id = null) {
+    if (cloud_id) {
+      return Repository.get(`${resource}/cloud/${cloud_id}`);
     } else {
       return Repository.get(`${resource}`);
     }
