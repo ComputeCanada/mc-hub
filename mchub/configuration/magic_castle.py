ef5174c3d3b5f6640ab2bd3b3e50758bd3352bcd
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-03-06 15:05:42 -0500

    Bump MC version to 12.3.0

--- mchub/configuration/magic_castle.py
+++ mchub/configuration/magic_castle.py
@@ -9,25 +9,25 @@ STORAGE_SPACES = ["home", "project", "scratch"]
 MAGIC_CASTLE_PUPPET_CONFIGURATION_URL = (
     "https://github.com/ComputeCanada/puppet-magic_castle.git"
 )
-TERRAFORM_REQUIRED_VERSION = ">= 1.1.0"
+TERRAFORM_REQUIRED_VERSION = ">= 1.3.0"
 
-MAGIC_CASTLE_VERSION = environ.get("MAGIC_CASTLE_VERSION", "11.9.1")
+MAGIC_CASTLE_VERSION = environ.get("MAGIC_CASTLE_VERSION", "12.3.0")
 MAGIC_CASTLE_PATH = environ.get("MAGIC_CASTLE_PATH", "git")
 if MAGIC_CASTLE_PATH == "git":
     MAGIC_CASTLE_SOURCE = {
-        "openstack" : f"git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref={MAGIC_CASTLE_VERSION}",
+        "openstack": f"git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref={MAGIC_CASTLE_VERSION}",
         "dns": {
-            "cloudflare" : f"git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref={MAGIC_CASTLE_VERSION}",
-            "gcloud" : f"git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud?ref={MAGIC_CASTLE_VERSION}"
-        }
+            "cloudflare": f"git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref={MAGIC_CASTLE_VERSION}",
+            "gcloud": f"git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud?ref={MAGIC_CASTLE_VERSION}",
+        },
     }
 else:
     MAGIC_CASTLE_SOURCE = {
-        "openstack" : path.join(".", "openstack"),
-        "dns" : {
-            "cloudflare" : path.join(".", "dns", "cloudflare"),
-            "gcloud" : path.join(".", "dns", "gcloud")
-        }
+        "openstack": path.join(".", "openstack"),
+        "dns": {
+            "cloudflare": path.join(".", "dns", "cloudflare"),
+            "gcloud": path.join(".", "dns", "gcloud"),
+        },
     }
 
 MAGIC_CASTLE_ACME_KEY_PEM = environ.get("MAGIC_CASTLE_ACME_KEY_PEM", "")
