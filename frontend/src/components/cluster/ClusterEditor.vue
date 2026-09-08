93c44664403ce4841ca898a664604d57e5192a18
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-09-08 09:01:14 -0400

    Move expiration date next to cloud project list

--- frontend/src/components/cluster/ClusterEditor.vue
+++ frontend/src/components/cluster/ClusterEditor.vue
@@ -3,21 +3,41 @@
     <v-form ref="form" v-model="validForm">
       <v-subheader>General configuration</v-subheader>
       <v-list class="pt-0">
-        <v-list-item v-if="!stateful">
-          <v-select
-            v-model="localSpecs.cloud.id"
-            item-value="id"
-            item-text="name"
-            :items="projects"
-            label="Cloud project"
-            @change="changeCloudProject"
-          />
-        </v-list-item>
-        <v-list-item v-else>
-          <v-list-item-content>
-            <v-list-item-subtitle>Cloud project</v-list-item-subtitle>
-            <v-list-item-title>{{ localSpecs.cloud.name }}</v-list-item-title>
-          </v-list-item-content>
+        <v-list-item>
+          <v-col cols="6" class="py-0">
+            <v-select
+              v-if="!stateful"
+              v-model="localSpecs.cloud.id"
+              item-value="id"
+              item-text="name"
+              :items="projects"
+              label="Cloud project"
+              @change="changeCloudProject"
+            />
+            <v-list-item-content v-else>
+              <v-list-item-subtitle>Cloud project</v-list-item-subtitle>
+              <v-list-item-title>{{ localSpecs.cloud.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-col>
+          <v-col cols="6" class="py-0">
+            <v-menu :nudge-right="40" transition="scale-transition" offset-y min-width="auto">
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="localSpecs.expiration_date"
+                  label="Expiration date"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="localSpecs.expiration_date"
+                @input="menu2 = false"
+                :min="tomorrowDate"
+              ></v-date-picker>
+            </v-menu>
+          </v-col>
         </v-list-item>
         <v-list-item v-if="!existingCluster">
           <v-col cols="6" class="py-0">
@@ -58,25 +78,6 @@
               <v-list-item-title>{{ localSpecs.version }}</v-list-item-title>
             </v-list-item-content>
           </v-col>
-        </v-list-item>
-        <v-list-item>
-          <v-menu :nudge-right="40" transition="scale-transition" offset-y min-width="auto">
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="localSpecs.expiration_date"
-                label="Expiration date"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="localSpecs.expiration_date"
-              @input="menu2 = false"
-              :min="tomorrowDate"
-            ></v-date-picker>
-          </v-menu>
         </v-list-item>
       </v-list>
       <v-divider />
